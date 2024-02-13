85259fbed782d75673d3c873c2ac012f66707b0c
zolppy <gabriel.lcifba@gmail.com>
2024-02-13 14:03:31 -0300

    script finalizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,20 @@
-const textEl = document.querySelector("textarea");
-const wordsEl = document.querySelector(".words");
-const charactersEl = document.querySelector(".characters");
+const textField = document.querySelector("textarea");
 
 const clearField = (field) => field.value = field.value && "";
 
-window.addEventListener("load", () => clearField(textEl));
+const updateWords = (textField, outputEl) => {
+  outputEl.textContent = (textField.value.trim() && textField.value.trim().split(" ").length) || "0";
+}
+
+const updateCharacters = (textField, outputEl) => {
+  outputEl.textContent = textField.value.length;
+}
+
+window.addEventListener("load", () => clearField(textField));
+textField.addEventListener("input", function() {
+  const wordsEl = document.querySelector(".words");
+  const charactersEl = document.querySelector(".characters");
+
+  updateWords(this, wordsEl);
+  updateCharacters(this, charactersEl);
+});
